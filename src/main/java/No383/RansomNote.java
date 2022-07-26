3409e991fa6af9a4bbbb02a3e0506f6b55b61cd5
hadron <2902511578@qq.com>
2022-07-26 21:11:47 +0800

    :three::eight::three: ransom-note
    https://leetcode.cn/problems/ransom-note/submissions/

--- src/main/java/No383/RansomNote.java
+++ src/main/java/No383/RansomNote.java
@@ -6,14 +6,53 @@ import java.util.Map;
 public class RansomNote {
 
 	public boolean canConstruct(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) { // (1)
+			return false;
+		}
 		Map<Integer, Integer> map = new HashMap<>();
-		magazine.chars().forEach(i -> map.merge(i, 1, Integer::sum));
-		return ransomNote.chars().noneMatch(i -> {
-			Integer value = map.get(i);
-			if (value != null && value != 0) {
-				map.put(i, value - 1);
+		magazine.chars().forEach(i -> map.merge(i, 1, Integer::sum));  // (2)
+		return ransomNote.chars().noneMatch(i -> { // (3)
+			Integer count = map.get(i);
+			if (count == null || count == 0) {
+				return true;
 			}
-			return value == null || value == 0;
+			map.put(i, count - 1);
+			return false;
 		});
+	}
+
+	public boolean canConstruct1(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) { // (1)
+			return false;
+		}
+		char base = 'a';
+		int[] count = new int[26];
+		for (char c : magazine.toCharArray()) { // (2)
+			count[c - base]++;
+		}
+		return ransomNote.chars()
+			.map(i -> { // (3)
+				count[i - base]--;
+				return count[i - base];
+			})
+			.noneMatch(i -> i < 0); // (4)
+	}
+
+	public boolean canConstruct2(String ransomNote, String magazine) {
+		if (ransomNote.length() > magazine.length()) { // (1)
+			return false;
+		}
+		char base = 'a';
+		int[] count = new int[26];  // (2)
+		for (char c : magazine.toCharArray()) {
+			count[c - base]++; // (3)
+		}
+		for (char c : ransomNote.toCharArray()) {
+			count[c - base]--;  // (4)
+			if (count[c - base] < 0) {  // (5)
+				return false;
+			}
+		}
+		return true;
 	}
 }
